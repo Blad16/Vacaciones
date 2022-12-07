90ff46095861df54662cfc521f8f8e9510f83f41
Blad16 <abrahambladimir400@gmail.com>
2022-12-07 16:22:48 -0600

    eliminar terminado botones

--- appVacaciones/src/app/mantenimiento/empleado/empleado.component.ts
+++ appVacaciones/src/app/mantenimiento/empleado/empleado.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { EmpleadoCargoControllerService } from 'src/app/api/services';
+import { NzMessageModule, NzMessageService, NzMNService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-empleado',
@@ -9,10 +10,18 @@ import { EmpleadoCargoControllerService } from 'src/app/api/services';
 export class EmpleadoComponent {
   empleado:EmpleadoComponent[]=[];
   constructor(
-    private empleadoService:EmpleadoCargoControllerService
+    private empleadoService:EmpleadoCargoControllerService,
+    private messageService:NzMessageService
   ) { }
   ngOnInit() {
     
   }
 
+
+  eliminar(id?:string): void{
+    this.messageService.info('su reigistro ha sido eliminado')
+  }
+  cancelar(): void{
+this.messageService.info('su reigistro sigue activo')
+   }
 }
